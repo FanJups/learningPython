9a40522f75bbdebdf08c5b0d786a46139a0e08e6
FanJups <jupsfan@gmail.com>
2019-05-10 21:14:45 +0200

    fractions

---
+++ fractions.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 10 20:56:28 2019
+
+@author: Admin
+"""
+
+import fractions
+a= fractions.Fraction(2,3)
+b= fractions.Fraction(1,2)
+print(a+b)
+
+print(type(a))
+
+print(isinstance(a,fractions.Fraction))
+
+print (isinstance(a,int))
